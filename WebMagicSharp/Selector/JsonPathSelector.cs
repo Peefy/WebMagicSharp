0b2c2638288a10a1015ef0a0bea0a308486c50f0
Peefy <xpf6677@163.com>
2017-12-26 14:57:21 +0800

    1
    1

--- WebMagicSharp/Selector/JsonPathSelector.cs
+++ WebMagicSharp/Selector/JsonPathSelector.cs
@@ -1,10 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace WebMagicSharp.Selector
 {
-    public class JsonPathSelector
+    /// <summary>
+    /// Json path selector.
+    /// </summary>
+    public class JsonPathSelector : ISelector
     {
-        public JsonPathSelector()
+        string jsonPathString;
+
+        public JsonPathSelector(string jsonPathString)
         {
+            this.jsonPathString = jsonPathString;
         }
+
+        public string Select(string text)
+        {
+            var jobjs = JObject.Parse(text).SelectTokens(jsonPathString);
+            if (jobjs == null)
+            {
+                return null;
+            }
+            return ToJsonString(jobjs.FirstOrDefault());
+        }
+
+        public List<string> SelectList(string text)
+        {
+            var list = new List<String>();
+            var jobjs = JObject.Parse(text).SelectTokens(jsonPathString);
+            if (jobjs == null)
+            {
+                return null;
+            }
+            foreach(var jobj in jobjs)
+            {
+                list.Add(ToJsonString(jobj));
+            }
+            return list;
+        }
+
+        public string ToJsonString(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~JsonPathSelector() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
