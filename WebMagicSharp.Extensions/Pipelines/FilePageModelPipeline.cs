fe0493ec8c1dbdf2b0c98f6d6f050907a7aed103
Peefy <xpf6677@163.com>
2018-01-09 17:24:03 +0800

    windows one

---
+++ WebMagicSharp.Extensions/Pipelines/FilePageModelPipeline.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebMagicSharp.Pipelines
+{
+    public class FilePageModelPipeline
+    {
+    }
+}
--- WebMagicSharp.Extensions/Scheduler/RedisScheduler.cs
+++ WebMagicSharp.Extensions/Scheduler/RedisScheduler.cs
@@ -2,9 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 
+using WebMagicSharp.Scheduler;
+
 namespace WebMagicSharp.Scheduler
 {
-    public class RedisScheduler
+    public class RedisScheduler : DuplicateRemovedScheduler, IMonitorableScheduler, IDuplicateRemover
     {
+        public int GetLeftRequestsCount(ITask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalRequestsCount(ITask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsDuplicate(Request request, ITask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetDuplicateCheck(ITask task)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
